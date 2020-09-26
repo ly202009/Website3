d73d603951399b4aae3bafd65f27bb416a36a716
Lee Yayoh <leeyayoh@gmail.com>
2020-09-25 20:01:30 -0600

    v_2020-09-25:20:01:30.11

--- js/10randqs.js
+++ js/10randqs.js
@@ -24,17 +24,17 @@ var roundedanswerNum = undefined, reportdiv = undefined;
     var submitbtn = await addbutton()
     $(submitbtn).text('next').css({ 'font-size': '30px' })
 
-    // 4. make a report div
-    reportdiv = await adddiv()
-    $(reportdiv).css({'font-family': 'Georgia', 'font-size': '30px'})
-    $(reportdiv).text('Report to be made after test is done.')
-    
-    // 5. on click of the submit button, match user's answer with the correct answer, make statistics on the # of acumulated correct answers
+    // 4. on click of the submit button, match user's answer with the correct answer, make statistics on the # of acumulated correct answers
     $(submitbtn).click(verifytheanswerandmoveon)
+
+    // 5. make a report div
+    reportdiv = await adddiv()
+    $(reportdiv).css({ 'font-family': 'Georgia', 'font-size': '30px' })
+    $(reportdiv).text('Report to be made after test is done.')
 
     //6. ask a question and verify
     await askAqNVerify()
-    
+
 
 })()
 
