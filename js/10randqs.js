5e7ec74413033272b4b246e39bfe35f9689bc140
Lee Yayoh <leeyayoh@gmail.com>
2020-09-25 18:19:10 -0600

    v_2020-09-25:18:19:10.41

--- js/10randqs.js
+++ js/10randqs.js
@@ -1,51 +1,19 @@
 const body = document.body;
 var divs=[], tmpdiv = undefined;
 
-for (var i =0; i < 2; i++) {
-    var tmpdiv = adddiv()
-    console.log (tmpdiv)
-    tmpdiv.setAttribute('class', 'equation')
-    divs.push(tmpdiv)
-}
-divs[0].textContent='1'
-divs[1].textContent='2'
+(async ()=>{
 
-// (async ()=>{
+    // console.log(tmpdiv)
+    // // make five divs
+    for (var i = 0; i < 5; i++) {
 
-//     // console.log(tmpdiv)
-//     // // make five divs
-//     for (var i = 0; i < 5; i++) {
-//         // console.log(i)
-//         var tmpdiv = await adddiv()
-//         tmpdiv.setAttribute('class', 'equation')
-//         divs.push(tmpdiv)
-//     }
-//     // divs[0].textContent='1'
-//     // divs[1].textContent='2'
+        var tmpdiv = await adddiv()
 
-// })()
+        tmpdiv.setAttribute('class', 'equation')
+        console.log('the tmp div here', tmpdiv)
+        divs.push(tmpdiv)
+    }
 
+})()
 
-
-
-// let div1 = adddiv()
-// div1.setAttribute('class', 'equation')
-
-// let div2 = adddiv()
-// div2.setAttribute('class', 'equation')
-
-// let div3 = adddiv()
-// div3.setAttribute('class', 'equation')
-
-// let div4 = adddiv()
-// div4.setAttribute('class', 'equation')
-
-// div4.innerText = '='
-
-// let div5 = adddiv()
-// div5.setAttribute('class', 'equation')
-
-
-
-// div5.setAttribute('contentEditable', true)
-// div5.addEventListener('input', checkanswer)
+console.log(tmpdiv)
--- js/commontools.js
+++ js/commontools.js
@@ -1,4 +1,4 @@
- function adddiv() {
+ async function adddiv() {
     let div = document.createElement('div')
     body.appendChild(div)
     return div
