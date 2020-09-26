bc0664920487b1d9266a171afab3decca52e5e73
Lee Yayoh <leeyayoh@gmail.com>
2020-09-25 18:22:48 -0600

    v_2020-09-25:18:22:48.62

--- js/10randqs.js
+++ js/10randqs.js
@@ -6,14 +6,17 @@ var divs=[], tmpdiv = undefined;
     // console.log(tmpdiv)
     // // make five divs
     for (var i = 0; i < 5; i++) {
-
         var tmpdiv = await adddiv()
-
         tmpdiv.setAttribute('class', 'equation')
-        console.log('the tmp div here', tmpdiv)
+        // console.log('the tmp div here', tmpdiv)
         divs.push(tmpdiv)
     }
 
+    div[3].innerText = '='
+
+
+
+
+
 })()
 
-console.log(tmpdiv)
